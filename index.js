6ccceaf70ae0d5bdfdc0303024ca301b25eaca9e
liguancong <liguancong@styd.cn>
2019-10-31 18:13:03 +0800

    update

--- index.js
+++ index.js
@@ -21,7 +21,13 @@ module.exports = {
             pathFile = configOption && configOption.favicon;
             if (pathFile) {
                 var faviconPath = path.join(process.cwd(), pathFile);
-                var gitbookFaviconPath = path.join(process.cwd(), output, 'gitbook', 'images', 'favicon.ico');
+                var gitbookFaviconPath = path.join(
+                  process.cwd(),
+                  output,
+                  "gitbook",
+                  "gitbook-plugin-theme-fexa",
+                  "favicon.ico"
+                );
                 if (fs.existsSync(faviconPath)) {
                     fs.writeFileSync(gitbookFaviconPath, fs.readFileSync(faviconPath));
                 }
@@ -31,7 +37,13 @@ module.exports = {
             pathFile = configOption && configOption.appleTouchIconPrecomposed152;
             if (pathFile) {
                 var appleTouchIconPrecomposed152 = path.join(process.cwd(), pathFile);
-                var gitbookAppleTouchPath = path.join(process.cwd(), output, 'gitbook', 'images', 'apple-touch-icon-precomposed-152.png');
+                var gitbookAppleTouchPath = path.join(
+                  process.cwd(),
+                  output,
+                  "gitbook",
+                  "images",
+                  "apple-touch-icon-precomposed-152.png"
+                );
                 if (fs.existsSync(appleTouchIconPrecomposed152)) {
                     fs.writeFileSync(gitbookAppleTouchPath, fs.readFileSync(appleTouchIconPrecomposed152));
                 }
@@ -41,7 +53,13 @@ module.exports = {
             pathFile = configOption && configOption.logo;
             if(pathFile){
                 var logoPath = path.join(process.cwd(), pathFile);
-                var pluginLogoPath = path.join(process.cwd(), output, 'gitbook','gitbook-plugin-theme-fexa',"logo.png");
+                var pluginLogoPath = path.join(
+                  process.cwd(),
+                  output,
+                  "gitbook",
+                  "gitbook-plugin-theme-fexa",
+                  "logo.png"
+                );
                 if (fs.existsSync(logoPath)) {
                     fs.writeFileSync(pluginLogoPath, fs.readFileSync(logoPath));
                 }
